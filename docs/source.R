67624cf64ba837b10d4b2bf188069d59935ecbf0
Ian Brettell <brettell@ebi.ac.uk>
2021-07-27 17:03:11 +0100

    20210727 02 update from local

--- docs/source.R
+++ docs/source.R
@@ -4,3 +4,14 @@
 
 library(tidyverse)
 library(DT)
+library(ggbeeswarm)
+
+######################
+# Plotting
+######################
+
+source("https://gist.githubusercontent.com/brettellebi/c5015ee666cdf8d9f7e25fa3c8063c99/raw/91e601f82da6c614b4983d8afc4ef399fa58ed4b/karyoploteR_lighter_darker.R")
+
+parental_lines = c("11-2", "14-2", "60-1", "72-1")
+pal_ror = c("#5F0F40", "#9a031e", "#fb8b24", "#0F4C5C")
+names(pal_ror) = parental_lines
